b12da1e52240edfa1bc4bff7368d9ac617236098
Connor Genyk <genyk-c@webmail.uwinnipeg.ca>
2017-10-12 11:51:10 -0500

    Hello Darkness My old Friend

---
+++ src/PgInfo.java
@@ -0,0 +1,13 @@
+public class PgInfo {
+
+    public String inbanner() {
+
+        System.out.println("===========================");
+        System.out.println("Developer: Connor Genyk");
+        System.out.println("Class: CS30");
+        System.out.println("Assignment: Lab Ex 2.1");
+        System.out.println("===========================");
+        return inbanner();
+    }
+}
+
